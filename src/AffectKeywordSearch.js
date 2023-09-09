8d3036a722bcecc15cff203aeb8e0e5a2bf7678a
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-09-08 18:35:30 -0700

    Functionality for adding keywords and affects in AffectKeywordSearch component

--- src/AffectKeywordSearch.js
+++ src/AffectKeywordSearch.js
@@ -5,20 +5,56 @@ export default function AffectKeywordSearch() {
     const keywordRef = useRef(null);
     const [results, setResults] = useState([]);
 
+    async function addAffect(keywordId) {
+        const requirement = Number(window.prompt("Enter requirement", 1));
+        setResults(
+            [(await graphQLQuery(`
+            mutation AddAffect($keywordId: Int!, $requirement: Int!) {
+                addAffect(keywordId: $keywordId, requirement: $requirement) {
+                    requirement, id
+                }
+            }`, { keywordId, requirement })).data.addAffect]
+        );
+    }
+
     async function handleSubmit(e) {
         e.preventDefault();
 
-        const keyword = keywordRef.current.value;
+        const word = keywordRef.current.value;
 
-        setResults((await graphQLQuery(`
+        const keyword = (await graphQLQuery(`
         query AffectsByKeyword($word: String!) {
             keywordByWord(word: $word) {
+                id,
                 affects {
                     requirement, id
                 }
             }
         }
-        `, { word: keyword })).data.keywordByWord.affects);
+        `, { word })).data.keywordByWord;
+
+        if (!keyword) {
+            if (window.confirm("No keyword with that search term exists. Would you like to create it?")) {
+                const keywordId = (await graphQLQuery(`
+                mutation AddKeyword($word: String!) {
+                    addKeyword(word: $word) {
+                        id
+                    }
+                }`, { word })).data.addKeyword.id;
+
+                if (window.confirm("Keyword created. Would you like to create an Affect associated with that keyword?")) {
+                    addAffect(keywordId);
+                }
+            }
+        } else {
+            if (keyword.affects.length > 0) {
+                setResults(keyword.affects);
+            } else {
+                if (window.confirm("Keyword exists, but there are no Affects associated with it. Would you like to create one?")) {
+                    addAffect(keyword.id);
+                }
+            }
+        }
     }
 
     return (
